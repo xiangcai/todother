85a9855851033a6d84dbdb406651a828fcb2124c
puckjam <puckjam@outlook.com>
2013-11-06 05:42:48 +0000

    Merged new interface.

--- controller/user.py
+++ controller/user.py
@@ -10,9 +10,5 @@ from controller.base import *
 class HomeHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self, username):
-        self.write('<html><body>Welcome back ' + self.current_user.nickname +
-                   '<a href="/todo_list">My Todo</a> '
-                   '<a href="/todo_achieve_list">My Achievement</a> '
-                   '<a href="/todo_giveup_list">My Giveup</a> '
-                   '<a href="/logout">Logout</a></body></html>')
+        self.render('home.html', username=self.current_user.nickname)
 
